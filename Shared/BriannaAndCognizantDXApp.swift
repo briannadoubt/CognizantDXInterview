33bff8209c64ef4dc5512d2d647acef06b59f6f0
Brianna Doubt <bri@briannadoubt.com>
2022-02-10 10:13:51 -0800

    Initial Commit

---
+++ Shared/BriannaAndCognizantDXApp.swift
@@ -0,0 +1,17 @@
+//
+//  BriannaAndCognizantDXApp.swift
+//  Shared
+//
+//  Created by Bri on 2/10/22.
+//
+
+import SwiftUI
+
+@main
+struct BriannaAndCognizantDXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
